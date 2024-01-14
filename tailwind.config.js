fa26218887cff83ba57cf792db20023e295a4961
Sima <sima.jazaa@gmail.com>
2024-01-15 00:28:10 +0300

    Feat:Pateints Views

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./templates/**/*.{html,js,py}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0D5CA5", // Customize with your preferred color
+        secondary: "#84BFF5",
+        blue_300: "#C8E0F6",
+        green_300: "#81BC78",
+        yellow: "#EFEB8D",
+        red: "#CA4747",
+        green: "#66C057",
+        black: "#282826",
+        blue_100: "#F7FAFD",
+
+        // Add more custom colors as needed
+      },
+      fontFamily: {
+        custom: ["roboto", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
