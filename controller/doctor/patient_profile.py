a73ba8ddf5eec1c7146ad18bf7b83a78045ee986
SaraZuhair <90957447+SaraZuhair@users.noreply.github.com>
2024-01-15 23:20:05 +0300

    updated

--- controller/doctor/patient_profile.py
+++ controller/doctor/patient_profile.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 
 doctor_profile_controller = Blueprint('patient_profile', __name__)
 
-@doctor_profile_controller.route('/doctor/profile')
+@doctor_profile_controller.route('/doctor/patient_profile')
 def doctor_route():
     return render_template('/doctor/patient_profile.html')
